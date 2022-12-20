7d4d762c7a3b61c76ea7ba586ecd29c13f69f6f8
AKASH <akash.arthanur26@gmail.com>
2022-12-20 16:16:05 +0530

    created get contact route and serach contact routes

--- routes/contactsRoute.js
+++ routes/contactsRoute.js
@@ -1,5 +1,6 @@
 const express = require("express")
-
+const mongoose = require("mongoose")
+const ObjectId = mongoose.Types.ObjectId
 const router = express.Router()
 const { Contact } = require("../models/contactSchema")
 const body_parser = require("body-parser")
@@ -8,7 +9,7 @@ router.post("/import", async (req, res) => {
     try {
         const data = req.body
         data.forEach((item) => {
-            item.user = "63a15b729d52ce44a15042d8"
+            item.user = "63a15b279d52ce44a15042d5"
         })
         const contact = await Contact.create(data)
         res.json(contact)
@@ -24,8 +25,8 @@ router.delete("/delete", async (req, res) => {
     try {
         if (selectedIDs.length) {
             selectedIDs.forEach(async (item) => {
-                const post =  await Contact.find({ _id: item })
-                if (post.length!=0) {
+                const post = await Contact.find({ _id: item })
+                if (post.length != 0) {
                     await Contact.deleteOne({ _id: item })
                     res.json({
                         message: "deleted succesfully"
@@ -48,4 +49,37 @@ router.delete("/delete", async (req, res) => {
     }
 
 })
+
+router.get("/", async (req, res) => {
+    try {
+        const contacts = await Contact.find({
+            user: "63a15b279d52ce44a15042d5"
+        })
+        res.status(200).json({
+            status: "Fetched",
+            contacts: contacts
+        })
+    } catch (e) {
+        res.status(401).json({
+            status: "failed to fetch",
+            message: e.message
+        })
+    }
+})
+
+router.post("/search", async (req, res) => {
+    try {
+        const contacts = await Contact.find({ Email: req.body.Email })
+        res.status(200).json({
+            status: "Fetched",
+            contacts: contacts
+        })
+    } catch (e) {
+        res.status(401).json({
+            status: "failed to fetch",
+            message: e.message
+        })
+    }
+})
+
 module.exports = router
